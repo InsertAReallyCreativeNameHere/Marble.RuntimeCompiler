8b01bb992da03b28d29b0ac3295187d427b3b424
InsertAReallyCreativeNameHere <60645493+InsertAReallyCreativeNameHere@users.noreply.github.com>
2021-06-24 19:15:20 +1000

    Update main.cpp

--- testexecutable/main.cpp
+++ testexecutable/main.cpp
@@ -19,34 +19,6 @@ int main()
     i = RuntimeCompiler::evaluate<long>("return 69;");
     std::cout << i << std::endl;
     i = RuntimeCompiler::evaluate<unsigned>("return 1337;");
-    std::cout << i << std::endl;
-    RuntimeCompiler::evaluate<void>
-    (
-        R"(
-            cout << "wow" << endl;
-        )",
-        CppCompileOptions()
-        .withIncludeDirectories
-        ({
-            "D:\\Github\\CykaBlyat-Tools\\mingw32\\i686-w64-mingw32\\include\\c++\\i686-w64-mingw32",
-            "D:\\Github\\CykaBlyat-Tools\\mingw32\\i686-w64-mingw32\\include\\c++\\backward",
-            "D:\\Github\\CykaBlyat-Tools\\mingw32\\i686-w64-mingw32\\include\\c++\\10.3.0",
-            "D:\\Github\\CykaBlyat-Tools\\mingw32\\i686-w64-mingw32\\include\\c++\\10.3.0\\i686-w64-mingw32",
-            "D:\\Github\\CykaBlyat-Tools\\mingw32\\i686-w64-mingw32\\include\\c++\\10.3.0\\backward",
-            "D:\\Github\\CykaBlyat-Tools\\mingw32\\include\\c++\\10.3.0",
-            "D:\\Github\\CykaBlyat-Tools\\mingw32\\include\\c++\\10.3.0\\i686-w64-mingw32",
-            "D:\\Github\\CykaBlyat-Tools\\mingw32\\include\\c++\\10.3.0\\backward",
-            "D:\\Github\\CykaBlyat-Tools\\mingw32\\lib\\gcc\\i686-w64-mingw32\\10.3.0\\include\\c++",
-            "D:\\Github\\CykaBlyat-Tools\\mingw32\\lib\\gcc\\i686-w64-mingw32\\10.3.0\\include\\c++\\i686-w64-mingw32",
-            "D:\\Github\\CykaBlyat-Tools\\mingw32\\lib\\gcc\\i686-w64-mingw32\\10.3.0\\include\\c++\\backward",
-            "D:\\Github\\CykaBlyat-Tools\\mingw32\\lib\\clang\\12.0.0\\include",
-            "D:\\Github\\CykaBlyat-Tools\\mingw32\\i686-w64-mingw32/sys-root/mingw/include",
-            "D:\\Github\\CykaBlyat-Tools\\mingw32\\i686-w64-mingw32\\include",
-            "D:\\Github\\CykaBlyat-Tools\\mingw32\\include"
-        })
-        .withIncludes({ "iostream" })
-        .withUsings({ "namespace std" })
-    );
 
     system("pause");
 }
